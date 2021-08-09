bb1c1ee115ce244224740abf49ae5e3526a4389f
Jackie Young <jyoung4@wellesley.edu>
2021-08-09 08:26:34 -0400

    styling fixes

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -3,5 +3,5 @@
 class Idea < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :tagline, presence: true, length: { maximum: 500 }
-  validates :description, :resources, :value_delivered, :goal, :hours_estimate, presence: true
+  validates :submitter, :description, :resources, :value_delivered, :goal, :hours_estimate, presence: true
 end
