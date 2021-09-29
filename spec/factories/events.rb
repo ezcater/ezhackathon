3f0d9d0dd696051956654e54f3ab39f8e4d52fdb
Daniel Patterson <daniel.patterson@ezcater.com>
2021-09-29 11:26:28 -0400

    Fix factories

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-# FactoryBot.define do
-#     factory :event do
-#       sequence(:start_time) { |n| DateTime.new(n) }
-#       end_time { DateTime.now }
-#       place { "Virtual" }
-#       agenda { "10am hacking"}
-#       organizers { "Rachael" }
-#     end
-#   end
+FactoryBot.define do
+  factory :event do
+    time { DateTime.now }
+    place { "Virtual" }
+    agenda { "10am hacking" }
+    participants { "Rachael" }
+    demo_links { "" }
+  end
+end
