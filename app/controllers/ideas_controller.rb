29c2ebcd35c63ee6c2290a9c8c37f00c1b8405ed
Daniel Patterson <daniel.patterson@ezcater.com>
2021-09-29 14:10:11 -0400

    Ensure flash messages are on all pages

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -16,7 +16,7 @@ class IdeasController < ApplicationController
       flash[:success] = "Idea successfully added!"
       redirect_to idea_path(@idea)
     else
-      flash.now[:errors] = @idea.errors.full_messages.to_sentence
+      flash.now[:errors] = @idea.errors.full_messages
       render :new
     end
   end
@@ -32,7 +32,7 @@ class IdeasController < ApplicationController
       flash[:success] = "Idea successfully updated!"
       redirect_to idea_path(@idea)
     else
-      flash.now[:errors] = @idea.errors.full_messages.to_sentence
+      flash.now[:errors] = @idea.errors.full_messages
       render :edit
     end
   end
