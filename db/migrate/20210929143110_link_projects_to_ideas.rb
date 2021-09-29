810ec0124b3b0c316dfc9b11a5a3c35094a974a4
Daniel Patterson <daniel.patterson@ezcater.com>
2021-09-29 11:50:11 -0400

    Fix linting

--- db/migrate/20210929143110_link_projects_to_ideas.rb
+++ db/migrate/20210929143110_link_projects_to_ideas.rb
@@ -4,6 +4,11 @@ class LinkProjectsToIdeas < ActiveRecord::Migration[6.0]
   disable_ddl_transaction!
 
   def change
+    # Rubocop wants this column to have a default value as it is now NOT NULL
+    # However, that doesn't make any sense for a reference / foreign key column
+    #
+    # rubocop:disable Rails/NotNullColumn
     add_reference :projects, :idea, index: { algorithm: :concurrently }, null: false
+    # rubocop:enable Rails/NotNullColumn
   end
 end
