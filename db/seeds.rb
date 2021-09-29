c9171532b4b3ace54b0d946489f09de59e0d58ca
Tristan Dunn <tristan.dunn@ezcater.com>
2021-09-29 14:20:53 -0400

    Fix the idea seeds and add seeds for awards and an event.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-["Cool app", "Silly app"].each do |name|
+Award.find_or_create_by!(title: "Culture cupcake")
+Award.find_or_create_by!(title: "Most likely to make it into production")
+
+Event.find_or_create_by!(participants: "Anyone!") do |event|
+  event.agenda = "Hack on some fun, new projects."
+  event.demo_links = "None yet."
+  event.place = "Anywhere"
+  event.time = 1.year.from_now
+end
+
+["Cool app", "Silly app", "Accepted app"].each do |name|
   Idea.find_or_create_by!(name: name) do |idea|
-    idea.tagline = "#{name} you probably want to work on!"
     idea.description = "A #{name.downcase} description."
-    idea.submitter = "Seed Database"
-    idea.resources = "Check out the wiki."
-    idea.hours_estimate = rand(32)
-    idea.value_delivered = "Everyone loves a #{name.downcase}."
     idea.goal = "To get people together and create a #{name.downcase}."
+    idea.hours_estimate = rand(32)
+    idea.resources = "Check out the wiki."
+    idea.submitter = "Seed Database"
+    idea.tagline = "#{name} you probably want to work on!"
+    idea.value_delivered = "Everyone loves a #{name.downcase}."
   end
 end
