0ccf17ba97c0042afb1c034e6bb3036419529cef
Daniel Patterson <daniel.patterson@ezcater.com>
2021-09-29 11:26:13 -0400

    Stop ignoring dropped columns

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,18 +1,6 @@
 # frozen_string_literal: true
 
 class Project < ApplicationRecord
-  # delegated to idea
-  self.ignored_columns = %w(
-    name
-    tagline
-    description
-    resources
-    snowflake_access
-    value_delivered
-    goal
-    hours_estimate
-  )
-
   belongs_to :event
   belongs_to :idea
 end
