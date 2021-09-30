062d334eca88dc979bd2deb43de2a2f7786036b3
Daniel Patterson <daniel.patterson@ezcater.com>
2021-09-30 09:40:41 -0400

    Refactor helper methods into strategy class
    So we could replace this with cookie- / authentication-based voting
    later.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,8 +4,7 @@ class VotesController < ApplicationController
   before_action :load_event
 
   def new
-    @username = params[:username]
-    @finished_voting = params[:finished_voting] || false
+    @strategy = ::Voting::UrlControlledVotingStrategy.new(params: params, event: @event)
   end
 
   private
@@ -13,14 +12,4 @@ class VotesController < ApplicationController
   def load_event
     @event = Event.find(params[:event_id])
   end
-
-  def user_started_voting?
-    @username.present?
-  end
-  helper_method :user_started_voting?
-
-  def user_finished_voting?
-    @finished_voting
-  end
-  helper_method :user_finished_voting?
 end
